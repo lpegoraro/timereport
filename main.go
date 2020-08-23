f24e2f8fcecc120d53846c82789dbe33dccb1def
Luiz Henrique Pegoraro <pegorarohluiz@gmail.com>
2020-08-23 11:32:10 -0300

    Testing mux

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import "os"
+
+var (
+	reportRepository repository.ReportRepository = repository.NewSQLiteRepository()
+	reportService    service.ReportService       = service.NewPostService(postRepository)
+	reportController controller.ReportController = controller.NewPostController(postService)
+	httpRouter       router.Router               = router.NewMuxRouter()
+)
 
 func main() {
-	fmt.Println("hello, world")
+	httpRouter.GET("/posts", postController.GetPosts)
+
+	httpRouter.POST("/posts", postController.AddPost)
+
+	httpRouter.SERVE(os.Getenv("PORT"))
 }
