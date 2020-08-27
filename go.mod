054e46d6c64b058848c139bed42168c077a23f16
Luiz Henrique Pegoraro <pegorarohluiz@gmail.com>
2020-08-27 15:34:11 -0300

    Without any code gen

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lpegoraro/timereport
+
+go 1.13
--- timereport.go
+++ timereport.go
@@ -1,10 +1,38 @@
 package reportstorage
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
 
-type server struct{}
-
-func (*server) ()
+const (
+	address     = "localhost:50051"
+)
 
 func main() {
-	handlerNewReportRepositoryClient()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := NewReportServiceClient(conn)
+
+	// Contact the server and print out its response.
+	reportMessage := "";
+	if len(os.Args) > 1 {
+		reportMessage = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.ReportEntity{Message: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
 }
+
